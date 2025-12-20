1e14014734b2b24e7039b66ee3587a6fac9a988b
OrdimixDj <56314042+OrdimixDj@users.noreply.github.com>
2025-12-20 02:23:48 +0100

    ex11.12_test2

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  this_causes_error
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
+})
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw ('error...  ')
+  res.send('ok')
 })
